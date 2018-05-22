b04ada32458e21f71b534c6f1d8aeeb5aae95ea4
306-san <oh.taku528+git@gmail.com>
2018-05-22 18:11:17 +0900

    動的経路のロジックを修正

--- app/controllers/onestrokes_controller.rb
+++ app/controllers/onestrokes_controller.rb
@@ -5,6 +5,16 @@ class OnestrokesController < ApplicationController
   def show
     p params[:first_end_station]
     p params
-    Station.find_by(name: '東京')
+    p onestroke_ids = Station.find_by(name: params[:first_end_station]).onestrokes.ids.uniq
+    onestroke_ids.reject! {|onestroke_id| OnestrokeStation.find_by(onestroke_id: onestroke_id, station: Station.find_by(name: params[:via_station])).nil? }
+    # onestroke_ids.each do |onestroke_id|
+    #   p onestroke_id
+    #    if OnestrokeStation.find_by(onestroke_id: onestroke_id, station: Station.find_by(name: params[:via_station])).nil?
+    #     onestroke_ids.delete(onestroke_id)
+    #    else
+    #     p "else id  " + onestroke_id.to_s
+    #   end
+    # end
+    @storoke_ids = onestroke_ids
   end
 end
