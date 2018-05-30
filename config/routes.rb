5a2e5587da17b79f0988c3328c7136d3c2d97789
306-san <oh.taku528+git@gmail.com>
2018-05-30 09:22:09 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'license/index'
   root 'onestrokes#index'
   get 'hoge/index'
   get 'onestrokes/index'
   get 'onestrokes/show'
+  get 'license' => 'license#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
